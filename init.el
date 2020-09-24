91cc8d984c2cb16ef058e12264a463dabc9bc9e8
Qiancheng Fu <qcfu@bu.edu>
2020-09-23 21:17:27 -0400

    more stuff

--- init.el
+++ init.el
@@ -20,7 +20,6 @@
 (setq-default indent-tabs-mode nil)
 (tooltip-mode 0)
 (tool-bar-mode 0)
-(scroll-bar-mode 0)
 (blink-cursor-mode 0)
 (save-place-mode 1)
 (global-auto-revert-mode t)
@@ -533,6 +532,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ (customize-set-variable 'scroll-bar-mode nil)
+ (customize-set-variable 'horizontal-scroll-bar-mode nil)
  )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
